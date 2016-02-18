c86ec256d33fbc38181db41d9f5d1a2c42dfdfbe
Formicarufa <Formicarufa@users.noreply.github.com>
2016-02-18 18:58:29 +0100

    Multiple graph colouring: color changed event

--- MultipleGraphsComponent/src/cz/dfi/graphsselectioncomponent/GraphedQuantity.java
+++ MultipleGraphsComponent/src/cz/dfi/graphsselectioncomponent/GraphedQuantity.java
@@ -1,19 +1,29 @@
 /*
  */
-
 package cz.dfi.graphsselectioncomponent;
 
 import cz.dfi.datamodel.graphable.GraphableQuantity;
 import java.awt.Color;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Objects;
 
 /**
  *
- * @author Tomas Prochazka
- * 21.12.2015
+ * @author Tomas Prochazka 21.12.2015
  */
 public final class GraphedQuantity {
 
+    public GraphableQuantity quantity;
+    private double translationX = 0;
+    private double translationY = 0;
+    private double scaleX = 1;
+    private double scaleY = 1;
+    private Color color;
+    private final List<PropertyChangeListener> listeners = Collections.synchronizedList(new LinkedList<PropertyChangeListener>());
 
     public GraphedQuantity(GraphableQuantity quantity) {
         this.quantity = quantity;
@@ -24,7 +34,9 @@ public final class GraphedQuantity {
     }
 
     public void setTranslationX(Double translationX) {
+        Object old = this.translationX;
         this.translationX = translationX;
+        fireEvent("translationX", old, translationX);
     }
 
     public double getTranslationY() {
@@ -32,7 +44,10 @@ public final class GraphedQuantity {
     }
 
     public void setTranslationY(Double translationY) {
+        Object old = this.translationY;
         this.translationY = translationY;
+        fireEvent("translationY", old, translationY);
+
     }
 
     public double getScaleX() {
@@ -40,7 +55,9 @@ public final class GraphedQuantity {
     }
 
     public void setScaleX(Double scaleX) {
+        Object old = this.scaleX;
         this.scaleX = scaleX;
+        fireEvent("scaleX", old, scaleX);
     }
 
     public double getScaleY() {
@@ -48,7 +65,9 @@ public final class GraphedQuantity {
     }
 
     public void setScaleY(Double scaleY) {
+        Object old = this.scaleY;
         this.scaleY = scaleY;
+        fireEvent("scaleY", old, scaleY);
     }
 
     public Color getColor() {
@@ -56,20 +75,18 @@ public final class GraphedQuantity {
     }
 
     public void setColor(Color color) {
+        Object old = this.color;
         this.color = color;
+        fireEvent("color", old, color);
     }
+
     public String getQuantityName() {
         return quantity.getName();
     }
+
     public String getQuantityUnit() {
         return quantity.getUnit();
     }
-    public GraphableQuantity quantity;
-    double translationX=0;
-    double translationY=0;
-    double scaleX=1;
-    double scaleY=1;
-    Color color;
 
     @Override
     public int hashCode() {
@@ -87,7 +104,22 @@ public final class GraphedQuantity {
             return false;
         }
         final GraphedQuantity other = (GraphedQuantity) obj;
-        return this.quantity==other.quantity;
+        return this.quantity == other.quantity;
     }
-    
+
+    private void fireEvent(String propName, Object old, Object newObj) {
+        PropertyChangeListener[] temp = listeners.toArray(new PropertyChangeListener[0]);
+        for (PropertyChangeListener l : temp) {
+            l.propertyChange(new PropertyChangeEvent(this, propName, old, newObj));
+        }
+    }
+
+    public void addPropertyChangedListener(PropertyChangeListener l) {
+        listeners.add(l);
+    }
+
+    public void removePropertyChangedListener(PropertyChangeListener l) {
+        listeners.remove(l);
+    }
+
 }
