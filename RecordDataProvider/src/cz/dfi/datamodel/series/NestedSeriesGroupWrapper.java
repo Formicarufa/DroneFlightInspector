be0df20872ed27405ad47e14bc90bec0e50d7f8c
Formicarufa <Formicarufa@users.noreply.github.com>
2016-03-05 12:16:24 +0100

    Hierarchical data model - series/values.

--- RecordDataProvider/src/cz/dfi/datamodel/series/NestedSeriesGroupWrapper.java
+++ RecordDataProvider/src/cz/dfi/datamodel/series/NestedSeriesGroupWrapper.java
@@ -8,6 +8,7 @@ import cz.dfi.datamodel.values.NestedValuesGroupWrapper;
 import cz.dfi.datamodel.values.ValueWrapper;
 import cz.dfi.datamodel.values.ValuesGroupWrapper;
 import java.util.Collection;
+import java.util.Collections;
 
 /**
  *
@@ -43,9 +44,28 @@ public class NestedSeriesGroupWrapper extends SeriesGroupWrapper {
         return groupWrapper;
     }
 
+    /**
+     * Returns a collection containing one ValuesGroupWrapper which contains ValueWrappers
+     * of children values. If children provide no values, returns an empty collection, instead.
+     * Ensure that returned ValueWrapper is put into the parent ValuesGroupWrapper.
+     * @param t1
+     * @param t2
+     * @param timeType
+     * @return 1 element collection or an empty collection 
+     */
     @Override
     public Collection<ValueWrapper> getIntervalSummary(long t1, long t2, TimeStampType timeType) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ValuesGroupWrapper groupWrapper = new NestedValuesGroupWrapper(name);
+        for (SeriesWrapper seriesWrapper : getChildren()) {
+            Collection<ValueWrapper> intervalSummary = seriesWrapper.getIntervalSummary(t1, t2, timeType);
+            for (ValueWrapper valueWrapper : intervalSummary) {
+                groupWrapper.addChild(valueWrapper);
+            }
+        }
+        if (groupWrapper.getChildren().isEmpty()) {
+            return Collections.EMPTY_LIST;
+        } 
+        return Collections.singleton(groupWrapper);
     }
 
 
--- RecordDataProvider/src/cz/dfi/datamodel/series/TopLevelSeriesGroupWrapper.java
+++ RecordDataProvider/src/cz/dfi/datamodel/series/TopLevelSeriesGroupWrapper.java
@@ -1,23 +1,24 @@
 /*
  */
-
 package cz.dfi.datamodel.series;
 
 import cz.dfi.datamodel.TimeStampType;
 import cz.dfi.datamodel.values.TimeInterval;
 import cz.dfi.datamodel.values.ValueWrapper;
 import cz.dfi.datamodel.values.ValuesGroupWrapper;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 /**
- *Series group wrapper which contains the time information.
- * The other implementation of {@link SeriesGroupWrapper}  does
- * not contain the time information itself. However, it has to have parent which
- * is able to provide the time information when needed.
- * Instance of this class does not have to have parent.
- * Class is instantiated through static method 
- * @author Tomas Prochazka
- * 28.2.2016
+ * Series group wrapper which contains the time information. The other
+ * implementation of {@link SeriesGroupWrapper} does not contain the time
+ * information itself. However, it has to have parent which is able to provide
+ * the time information when needed. Instance of this class does not have to
+ * have parent. Class is instantiated through static method
+ *
+ * @author Tomas Prochazka 28.2.2016
  */
 class TopLevelSeriesGroupWrapper extends SeriesGroupWrapper {
 
@@ -25,12 +26,14 @@ class TopLevelSeriesGroupWrapper extends SeriesGroupWrapper {
 
     /**
      * Creates a new group which joins multiple series of values together.
+     *
      * @param name Name of the group
-     * @param timeStamps Array of time stamps associated with the values of series subsumed into this group
+     * @param timeStamps Array of time stamps associated with the values of
+     * series subsumed into this group
      */
-     TopLevelSeriesGroupWrapper(String name, TimeStampArray timeStamps) {
+    TopLevelSeriesGroupWrapper(String name, TimeStampArray timeStamps) {
         super(name);
-        this.timeStamps=timeStamps;
+        this.timeStamps = timeStamps;
     }
 
     @Override
@@ -51,10 +54,18 @@ class TopLevelSeriesGroupWrapper extends SeriesGroupWrapper {
     @Override
     public Collection<ValueWrapper> getIntervalSummary(long t1, long t2, TimeStampType timeType) {
         ValuesGroupWrapper groupWrapper = ValuesGroupWrapper.create(name, getTimeStamps().getTimeInterval(t1, t2, timeType));
-        //TODO:Finish
-    }
-     
+        for (SeriesWrapper seriesWrapper : getChildren()) {
+            Collection<ValueWrapper> intervalSummary = seriesWrapper.getIntervalSummary(t1, t2, timeType);
+            for (ValueWrapper valueWrapper : intervalSummary) {
+                groupWrapper.addChild(valueWrapper);
+            }
+        }
+        if (groupWrapper.getChildren().isEmpty()) {
+            return Collections.EMPTY_LIST;
+        } else {
+            return Collections.singleton(groupWrapper);
+        }
 
-   
+    }
 
 }
